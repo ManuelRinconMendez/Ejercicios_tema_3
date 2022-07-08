36266d75ae13ecff8fa4deacf4804b7bc2d4bf2b
ciclo3manuelrincon <manarincon8@misena.edu.co>
2022-07-07 21:02:02 -0500

    Se inicia el ejercicio

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Sumar suma = new Sumar();
+        suma.sumar(1,2,4);
+
+    }
+
+}
+
+
---
+++ src/Sumar.java
@@ -0,0 +1,9 @@
+public class Sumar {
+
+    public void sumar(int a, int b, int c) {
+        int resultado = 0;
+        resultado = a + b + c;
+        System.out.println(resultado);
+        }
+}
+
